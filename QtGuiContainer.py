3ed3106bbf276b4f187a02785f62d6627812bb8a
jjcc <jjccforth@gmail.com>
2019-12-19 22:42:16 -0500

    init commit

---
+++ QtGuiContainer.py
@@ -0,0 +1,131 @@
+import sys
+import random
+from PySide2 import QtCore, QtWidgets
+import PySide2.QtWidgets
+
+from  PySide2.QtWidgets import *
+import vtk
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+
+qt_app = QApplication(sys.argv)
+
+
+
+class MyWidget(QVTKRenderWindowInteractor):
+    def __init__(self):
+        super().__init__()
+
+
+
+class LayoutExample(QWidget):
+    ''' An example of PySide/PyQt absolute positioning; the main window
+        inherits from QWidget, a convenient widget for an empty window. '''
+
+    def __init__(self):
+        # Initialize the object as a QWidget and
+        # set its title and minimum width
+        QWidget.__init__(self)
+        self.setWindowTitle('Dynamic Greeter')
+        self.setMinimumWidth(800)
+
+        vtkWidgit  = self.create_vtkw()
+        # Create the QVBoxLayout that lays out the whole form
+        self.layout = QVBoxLayout()
+
+        # Create the form layout that manages the labeled controls
+        self.form_layout = QFormLayout()
+
+        # The salutations that we want to make available
+        self.salutations = ['Ahoy',
+                            'Good day',
+                            'Hello',
+                            'Heyo',
+                            'Hi',
+                            'Salutations',
+                            'Wassup',
+                            'Yo']
+
+        # Create and fill the combo box to choose the salutation
+        self.salutation = QComboBox(self)
+        self.salutation.addItems(self.salutations)
+
+        # Add it to the form layout with a label
+        self.form_layout.addRow('Salutation:', self.salutation)
+
+        # Create the entry control to specify a recipient
+        # and set its placeholder text
+        self.recipient = QLineEdit( self)
+        self.recipient.setPlaceholderText( "e.g.'world' or 'Matey' ")
+
+        # Add it to the form layout with a label
+        self.form_layout.addRow('Recipient:', self.recipient)
+
+        # Create and add the label to show the greeting text
+        self.greeting = QLabel('', self)
+        self.form_layout.addRow('Greeting:', self.greeting)
+
+        # Add the form layout to the main VBox layout
+        self.layout.addLayout(self.form_layout)
+
+        # Add stretch to separate the form layout from the button
+        self.layout.addStretch(1)
+
+        # Create a horizontal box layout to hold the button
+        self.button_box = QHBoxLayout()
+
+        # Add stretch to push the button to the far right
+        self.button_box.addStretch(1)
+
+        # Create the build button with its caption
+        self.build_button = QPushButton('My Button', self)
+
+        # Add it to the button box
+        self.button_box.addWidget(self.build_button)
+
+        # Add the button box to the bottom of the main VBox layout
+        self.layout.addLayout(self.button_box)
+
+        self.layout.addWidget(vtkWidgit)
+
+        # Set the VBox layout as the window's main layout
+        self.setLayout(self.layout)
+
+    def run(self):
+        # Show the form
+        self.show()
+        # Run the qt application
+        qt_app.exec_()
+
+
+    def create_vtkw(self):
+        widget = MyWidget()  # QVTKRenderWindowInteractor()
+        widget.Initialize()
+        widget.Start()
+        # if you don't want the 'q' key to exit comment this.
+        widget.AddObserver("ExitEvent", lambda o, e, a=qt_app: a.quit())
+
+        ren = vtk.vtkRenderer()
+        widget.GetRenderWindow().AddRenderer(ren)
+
+        #Create source
+        source = vtk.vtkConeSource()
+        source.SetResolution(32)
+
+        # source = vtk.vtkSphereSource()
+        # source.SetCenter(0, 0, 0)
+        # source.SetRadius(5.0)
+
+        coneMapper = vtk.vtkPolyDataMapper()
+        coneMapper.SetInputConnection(source.GetOutputPort())
+
+        coneActor = vtk.vtkActor()
+        coneActor.SetMapper(coneMapper)
+
+        ren.AddActor(coneActor)
+        return widget
+
+
+
+# Create an instance of the application window and run it
+app = LayoutExample()
+app.run()
---
+++ QtGuiVtk.py
@@ -0,0 +1,125 @@
+import sys
+import random
+from PySide2 import QtCore, QtWidgets, QtGui
+
+import PySide2.QtCore
+import vtk
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(603, 553)
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.gridlayout = QtWidgets.QGridLayout(self.centralWidget)
+        self.vtkWidget = QVTKRenderWindowInteractor(self.centralWidget)
+        self.gridlayout.addWidget(self.vtkWidget, 0, 0, 1, 1)
+        #MainWindow.setCentralWidget(self.centralWidget)
+
+class MyWidget0(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ren = vtk.vtkRenderer()
+        self.ui.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
+        self.iren = self.ui.vtkWidget.GetRenderWindow().GetInteractor()
+
+        # Create source
+        source = vtk.vtkSphereSource()
+        source.SetCenter(0, 0, 0)
+        source.SetRadius(5.0)
+
+        # Create a mapper
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(source.GetOutputPort())
+
+        # Create an actor
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+
+        self.ren.AddActor(actor)
+
+        #
+        # self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "??"]
+        #
+        # self.button = QtWidgets.QPushButton("Click me!")
+        # self.text = QtWidgets.QLabel("Hello World")
+        # self.text.setAlignment(QtCore.Qt.AlignCenter)
+        #
+        # self.layout = QtWidgets.QVBoxLayout()
+        # self.layout.addWidget(self.text)
+        # self.layout.addWidget(self.button)
+        # self.setLayout(self.layout)
+        #
+        # self.button.clicked.connect(self.magic)
+
+
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+    def print_inf(self):
+        print(PySide2.__version__)
+        print(QtCore.__version__)
+
+class MyWidget(QVTKRenderWindowInteractor):
+    def __init__(self):
+        super().__init__()
+
+
+
+
+
+def QVTKRenderWidgetConeExample():
+
+
+
+    """A simple example that uses the QVTKRenderWindowInteractor class."""
+
+    # every QT app needs an app
+    app = QtWidgets.QApplication(['QVTKRenderWindowInteractor'])
+
+    # create the widget
+    widget = MyWidget()#QVTKRenderWindowInteractor()
+    widget.Initialize()
+    widget.Start()
+    # if you don't want the 'q' key to exit comment this.
+    widget.AddObserver("ExitEvent", lambda o, e, a=app: a.quit())
+
+    ren = vtk.vtkRenderer()
+    widget.GetRenderWindow().AddRenderer(ren)
+
+    #source = vtk.vtkConeSource()
+    #source.SetResolution(8)
+    # Create source
+    source = vtk.vtkSphereSource()
+    source.SetCenter(0, 0, 0)
+    source.SetRadius(5.0)
+
+
+    coneMapper = vtk.vtkPolyDataMapper()
+    coneMapper.SetInputConnection(source.GetOutputPort())
+
+    coneActor = vtk.vtkActor()
+    coneActor.SetMapper(coneMapper)
+
+    ren.AddActor(coneActor)
+
+    # show the widget
+    widget.show()
+    # start event processing
+    app.exec_()
+
+if __name__ == "__main__":
+
+
+    # app = QtWidgets.QApplication([])
+    #
+    # widget = MyWidget()
+    # widget.print_inf()
+    # widget.resize(800, 600)
+    # widget.show()
+    #
+    # sys.exit(app.exec_())
+    QVTKRenderWidgetConeExample()
